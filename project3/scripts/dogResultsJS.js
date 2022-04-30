00a0cd16f7d2293de89e4cc7e1ea66aa909634f4
eiliscasey17 <98776757+eiliscasey17@users.noreply.github.com>
2022-04-29 22:10:37 -0400

    Delete dogResultsJS.js

--- project3/scripts/dogResultsJS.js
+++
@@ -1,9 +0,0 @@
-let getDog = function(){
-    var request =  new XMLHttpRequest();
-    request.open("GET", "https://dog.ceo/api/breeds/image/random");
-    request.onload = function(){
-        var data = JSON.parse(this.response);
-        var dogImage =data.message;
-        document.getElementById(dog).imageContent=dogImage;
-    }
-}
